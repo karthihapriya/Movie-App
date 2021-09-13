3feb522439e4672a941ef63660d5bbe0db9a1bf4
karthihapriya <karthihapriya@gmail.com>
2021-09-13 21:28:21 +0530

    added the filter card and movie name,geners,artist,release date start and date end,apply button with style

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,62 +1,32 @@
 import React from "react";
-import Header from "../../common/header/Header"
-import "./Home.css"
-import ImageList from '@material-ui/core/ImageList';
-import moviesData from  "../../common/moviesData"
-import ImageListItem from '@material-ui/core/ImageListItem';
-import ImageListItemBar from '@material-ui/core/ImageListItemBar';
-import { makeStyles } from '@material-ui/core/styles';
-
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
-  imageList: {
-    flexWrap: 'nowrap',
-  },
-  title: {
-    color: "#fff",
-  },
-  titleBar: {
-    background:
-      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-  },
-}));
-
-
- const SingleLineImageList = function () {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <ImageList className={classes.imageList}  cols={6} rowHeight={180}>
-            {moviesData.map((item)=>(
-              <ImageListItem key={item.id}>
-                <img src={item.poster_url} alt={item.title} />
-                <ImageListItemBar 
-                  title={item.title}
-                  classes={{
-                    root: classes.titleBar,
-                    title: classes.title,
-                  }}
-                />
-              </ImageListItem>
-            ))}
-      </ImageList>
-    </div>
-  );
-}
-
-const dateConverter=dateEntry=>{
-  let myDate = new Date(dateEntry);
-  return myDate.toDateString();
-}
+import Header from "../../common/header/Header";
+import "./Home.css";
+import MovieList from "./MovieList";
+import MovieFilter from "./MovieFilter";
+import SingleLineImageList from "./SingleLineImageList";
 
 class Home extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      artist: [],
+      genre: [],
+      movieName: "",
+      releaseDateStart: "",
+      releaseDateEnd: ""
+    }
+  }
+  copyState = (s1) =>{
+    // console.log("This is ", this);
+    // console.log("BUt s1 is", s1);
+    this.setState({
+      artist: s1.artist.slice(),
+      genre: s1.genre.slice(),
+      movieName: s1.movieName,
+      releaseDateStart: s1.releaseDateStart,
+      releaseDateEnd: s1.releaseDateEnd
+    })
+  }
   render(){
     return(
       <div id="home-main">
@@ -65,17 +35,10 @@ class Home extends React.Component{
         <SingleLineImageList />
         <div className="flex-container">
           <div className="left">
-            <ImageList rowHeight={350} cols={4} gap={10}>
-              {moviesData.map((item)=>(
-                <ImageListItem key={item.id}>
-                  <img className="img-link" src={item.poster_url} alt={item.title} />
-                  <ImageListItemBar
-                  title={item.title}
-                  subtitle={<span>{"Release Date:  " + dateConverter(item.release_date)}</span>}
-                  />
-                </ImageListItem>
-              ))}
-            </ImageList>
+            <MovieList parameters={this.state} />
+          </div>
+          <div className="right">
+            <MovieFilter func={this.copyState} />
           </div>
         </div>
       </div>
